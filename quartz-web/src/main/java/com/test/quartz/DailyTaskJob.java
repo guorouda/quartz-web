2d34b4214d16642afb746792f16bee2d3d961de9
sunrong <sunrong@msn.com>
2014-11-26 15:42:41 +0800

    first commit.
    Signed-off-by: sunrong <sunrong@msn.com>

---
+++ quartz-web/src/main/java/com/test/quartz/DailyTaskJob.java
@@ -0,0 +1,15 @@
+package com.test.quartz;
+
+import org.apache.log4j.Logger;
+
+public class DailyTaskJob {
+	
+	static Logger log = Logger.getLogger(DailyTaskJob.class);
+	
+	protected void execute() {
+        //执行任务
+		log.info("**********************");
+		
+   }
+
+}
